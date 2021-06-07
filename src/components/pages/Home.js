340471964192df5ea4e17ddf03ead61e388c54e2
Kah Yen Lim <kahyen.lim1@hs-augsburg.de>
2021-06-07 12:15:15 +0200

    Implement basic homepage structure
    Removed unused css as well.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,3 @@
-import logo from "../../resources/images/logo.svg";
-
 /**
  * Main page of the application.
  * @return {JSX.Element}
@@ -7,22 +5,35 @@ import logo from "../../resources/images/logo.svg";
  */
 function Home() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <>
+        <div className="container">
+            <div className="row">
+                <h2>Startseite</h2>
+                <p>Herzlich Willkommen bei der WPF-Anmeldeseite!<br/>Auf der linken Seite finden Sie ale nötigen
+                    Informationen zur Anmeldung.</p>
+            </div>
+            <div className="row">
+                <h2>Anmeldefrist</h2>
+                <p>Die Anmeldefrist für die Wahlpflichtfächer beginnt am 29.4.2021 und endet am 30.05.2021.</p>
+            </div>
+            <div className="row">
+                <h2>Kontakt</h2>
+                <p>Bei Fragen/Problemen wenden Sie sich bitte an Frau Bäurle.</p>
+            </div>
+            <div className="row">
+                <h2>Impressum</h2>
+                <a href='/' style={{ marginBottom: '0.5rem' }}>Link</a>
+            </div>
+            <div className="row">
+                <h2>Übersicht Wahlpflichtfächer</h2>
+                <p>todo</p>
+            </div>
+            <div className="row">
+                <h2>Create registration</h2>
+                <button className="btn btn-primary" style={{ textAlign: 'center', marginLeft: '1em', width: '10%' }}>Submit</button>
+            </div>
         </div>
+        </>
     );
 }
 
