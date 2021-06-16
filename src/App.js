af3ef0483843fbb8d9cd89e97d9031b6a27e6711
Kah Yen Lim <kahyen.lim1@hs-augsburg.de>
2021-06-16 15:50:31 +0200

    Add basic structure of the subject overview page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './resources/css/App.css';
 import Home from './components/pages/Home';
+import SubjectOverview from "./components/pages/SubjectOverview";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
       <Router>
         <Route path='/' component={ Home } exact />
+          <Route path='/subjects' component={ SubjectOverview } />
       </Router>
   );
 }
