3fa8bf2d9d961baaa9aa7ec3b366ad201e70cf9e
Kah Yen Lim <kahyen.lim1@hs-augsburg.de>
2021-12-09 10:04:26 +0100

    Add runnable webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
     output: {
         filename: 'subject-registration-frontend.bundle.js',
     },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
     module: {
         rules: [
             {
@@ -14,6 +17,22 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-            }],
+            },
+            {
+                test: /\.css?$/,
+                loader: "css-loader"
+            },
+            {
+                test: /\.scss?$/,
+                use: [
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            }
+        ],
     },
 };
