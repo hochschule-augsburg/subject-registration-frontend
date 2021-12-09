ab9b766d09263cc672a70a4ddc0ea63acbfa52ae
Kah Yen Lim <kahyen.lim1@hs-augsburg.de>
2021-12-09 10:45:25 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\\.ts$/,
+                test: /\.tsx?$/,
                 loader: "ts-loader"
             },
             {
